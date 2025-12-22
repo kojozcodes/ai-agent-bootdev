d72dff2fd7bf7c5ee9f57a0f65d1ba1bf78ac68c
kojozcodes <kojozcodes@gmail.com>
2025-12-22 14:31:57 +0100

    store multiple messages and add calculator

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import argparse
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -9,11 +11,25 @@ def main():
     if api_key is None:
         raise RuntimeError("GEMINI_API_KEY not found in environment. Did you create your .env file?")
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model="gemini-2.5-flash", contents=messages
     )
+
+    if response.usage_metadata is None:
+        raise RuntimeError("Usage Metadata not available")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print(response.text)
 
 if __name__ == "__main__":
