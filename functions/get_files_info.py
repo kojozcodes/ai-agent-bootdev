53dfa972014bd3864f7bf8d21ccd80c8031c8590
kojozcodes <kojozcodes@gmail.com>
2025-12-23 22:11:46 +0100

    add functions to read files

---
+++ functions/get_files_info.py
@@ -0,0 +1,25 @@
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+
+        # Will be True or False
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        
+        content_list = []
+        for item in os.listdir(target_dir):
+            item_path = os.path.join(target_dir, item)
+            string_to_append = f"- {item}: file_size={os.path.getsize(item_path)} bytes, is_dir={os.path.isdir(item_path)}"
+            content_list.append(string_to_append)
+        return "\n".join(content_list)
+    except Exception as e:
+        return f"Error: {e}"
